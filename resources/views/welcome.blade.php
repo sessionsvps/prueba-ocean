99ddf740f5e90c267de78fb4534af03dba1e149c
Dejer <zulatecnico2020@gmail.com>
2024-06-11 15:24:50 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>EZEIZA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">EZEIZA</a>
+        </nav>
+    </header>
+
+    <main>
+        <div class="jumbotron">
+            <h1 class="display-4">Bienvenidos a EZEIZA</h1>
+            <p class="lead">Esto es una simple página de aterrizaje para la ciudad de EZEIZA.</p>
+        </div>
+    </main>
+
+    <footer class="page-footer font-small blue pt-4">
+        <div class="container-fluid text-center text-md-left">
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase">Pie de página</h5>
+                    <p>Aquí puedes poner cualquier cosa que quieras que aparezca en el pie de página.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
